cd4b48d7ea83b2e642fb0207eac56d2559c78302
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-02-23 16:00:20 +0000

    Update server

---
+++ server/main.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, send_file
+from PIL import Image
+import hashlib
+import uuid
+import json
+import os
+
+print("Reading config file...")
+config = {}
+try:
+    with open("./config.json", 'r') as file:
+        config = json.loads(file.read())
+except:
+    print("Could not read config file! - Generating new one")
+    config["auth_key"] = str(uuid.uuid4())
+    with open("./config.json", "w") as file:
+        file.write(json.dumps(config))
+    print(f"GENERATED AUTH KEY: {config['auth_key']}")
+
+def hashImage(image: Image.Image):
+    m = hashlib.sha256()
+    m.update(image.tobytes())
+    return m.hexdigest()
+
+app = Flask(__name__)
+
+os.makedirs("cache/images", exist_ok=True)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if (request.headers.get("authorization", "").split(" ")[-1] != config["auth_key"]):
+        return "UNAUTHORIZED", 403
+    
+    if (request.method == "POST"):
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return "Invalid Upload Request", 400
+        file = request.files['file']
+        image = Image.open(file.stream)
+        image_id = hashImage(image)
+        path = f"./cache/images/{image_id}.png"
+        new = False
+        if (not os.path.exists(path)):
+            image.save(path)
+            new = True
+        return {
+            "path": f"/{image_id}.png",
+            "new": new
+        }
+    return "<a href=\"https://github.com/ThatHackerDudeFromCyberspace/HDPresence\">HDPresence Server</a>"
+
+@app.route("/<image_filename>", methods=["GET", "DELETE"])
+def image_handler(image_filename):
+    if (request.headers.get("authorization", "").split(" ")[-1] != config["auth_key"]):
+        return "UNAUTHORIZED", 403
+    
+    for forbidden in ["\\", "'", '"', '.', '/']:
+        if (forbidden in image_filename):
+            return "INVALID REQUEST", 400
+    
+    image_path = os.path.join("./cache/images/", image_filename)
+    if (not os.path.exists(image_path)):
+        return "NOT FOUND", 404
+
+    if (request.method == "DELETE"):
+        os.remove(image_path)
+        return {}, 200
+    return send_file(image_path, "image/png", False)
+
+if (__name__ == "__main__"):
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8000)
