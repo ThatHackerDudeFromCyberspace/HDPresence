a5a595095f966b7e916cc0df57be9b1fb34dadd7
ThatHackerDudeFromCyberspace <69104218+ThatHackerDudeFromCyberspace@users.noreply.github.com>
2026-02-23 12:32:38 +0000

    Add multiple-player support to MPRIS handler

--- activity_handlers/mpris_handler.py
+++ activity_handlers/mpris_handler.py
@@ -10,6 +10,59 @@ import dbus
 from activity_handler import ActivityHandler, HandlerContext, HandlerResponse
 from discord_ipc import ACTIVITY_TYPE, DiscordActivity, DiscordActivityAssets, DiscordActivityButton, DiscordActivityImage, DiscordActivityTimestamps
 
+class Player():
+    def __init__(self,
+                 name: str,
+                 title: str|None,
+                 artists: list[str],
+                 cover_art_url: str|None,
+                 length: int,
+                 position: int,
+                 status: str
+                ):
+        self.name = name
+        self.title = title
+        self.artists = artists
+        self.cover_art_url = cover_art_url
+        self.length = length
+        self.position = position
+        self.status = status
+
+    def get_artist(self):
+        if (len(self.artists) == 0):
+            return None
+        return self.artists[0]
+    
+    def get_start(self):
+        if (self.status != "Playing" or self.position >= self.length):
+            return 1
+        else:
+            return time.time()*1000 - self.position
+    
+    def get_end(self):
+        if (self.status != "Playing" or self.position >= self.length):
+            return self.length/1000 + 1
+        else:
+            start = time.time()*1000 - self.position
+            return start + self.length
+        
+    def get_status(self):
+        return self.status
+    
+    def get_name(self):
+        return self.name
+    
+    def get_title(self):
+        return self.title
+    
+    def get_length(self):
+        return self.length
+
+    def get_hash(self):
+        return f"{str(self.get_status())}:{str(self.get_name())}:{self.get_title()}:{math.trunc(self.get_start()/10000)}:{self.get_length()}"
+
+    
+
 class MPRISHandler(ActivityHandler):
     SERVICE_PREFIX = "org.mpris.MediaPlayer2"
 
@@ -17,6 +70,7 @@ class MPRISHandler(ActivityHandler):
         self.session_bus = context.session_bus
 
     def get_activity(self) -> DiscordActivity:
+        players: list[Player] = []
         for bus_name in self.session_bus.list_names():
             bus_name = str(bus_name)
             if (bus_name[:len(self.SERVICE_PREFIX)] == self.SERVICE_PREFIX):
@@ -31,36 +85,52 @@ class MPRISHandler(ActivityHandler):
                 if (title != None):
                     title = str(title)
                 artists = metadata.get("xesam:artist", [])
-                artist = None
-                if (len(artists) > 0):
-                    artist = str(artists[0])
-                cover_art = metadata.get("mpris:artUrl", None)
-                length = metadata.get("mpris:length", 0) / 1000
+                cover_art_url = metadata.get("mpris:artUrl", None)
+                length = metadata.get("mpris:length", 1000000) / 1000
                 position = mediaplayer_properties.Get("org.mpris.MediaPlayer2.Player", "Position") / 1000
                 status = mediaplayer_properties.Get("org.mpris.MediaPlayer2.Player", "PlaybackStatus")
 
-                if (str(status) != "Playing" or position >= length):
-                    start = 1
-                    end = length/1000 + 1
-                else:
-                    start = time.time()*1000 - position
-                    end = start + length
-
-                return HandlerResponse(
-                    DiscordActivity(
-                        str(name),
-                        type = ACTIVITY_TYPE.LISTENING,
-                        details=title,
-                        details_url=f"https://www.youtube.com/results?search_query={urllib.parse.quote_plus(str(artist) + ' "' + str(title) + '"')}",
-                        state=f"{artist} | {str(status)}",
-                        timestamps=DiscordActivityTimestamps(
-                            start=start,
-                            end=end
-                        )
-                    ),
-                    hash=f"{str(status)}:{str(name)}:{title}:{math.trunc(start/10000)}:{length}"
+                players.append(
+                    Player(
+                        name,
+                        title,
+                        [str(artist) for artist in artists],
+                        cover_art_url,
+                        int(length),
+                        int(position),
+                        str(status)
+                    )
                 )
-        return HandlerResponse()
+
+        if (len(players) == 0):
+            return HandlerResponse()
+
+        selected_player = players[0]
+        for player in players:
+            if (player.get_status() == "Playing"):
+                selected_player = player
+                break
+            if (player.get_start() > selected_player.get_start()):
+                selected_player = player
+
+        if (selected_player.get_status() != "Playing"):
+            if (len(players) > 1):
+                return HandlerResponse()
+
+        return HandlerResponse(
+            DiscordActivity(
+                str(selected_player.get_name()),
+                type = ACTIVITY_TYPE.LISTENING,
+                details=selected_player.get_title(),
+                details_url=f"https://www.youtube.com/results?search_query={urllib.parse.quote_plus(str(selected_player.get_artist()) + ' "' + str(selected_player.get_title()) + '"')}",
+                state=f"{selected_player.get_artist()} | {str(selected_player.get_status())}",
+                timestamps=DiscordActivityTimestamps(
+                    start=selected_player.get_start(),
+                    end=selected_player.get_end()
+                )
+            ),
+            hash=selected_player.get_hash()
+        )
 
 
 
